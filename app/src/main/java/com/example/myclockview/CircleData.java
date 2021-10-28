c48c738ffb584470594ed806079bebfca7e06a80
liuml <liudao1@qq.com>
2021-10-28 17:33:01 +0800

    画出圆和刻度

---
+++ app/src/main/java/com/example/myclockview/CircleData.java
@@ -0,0 +1,45 @@
+package com.example.myclockview;
+
+/**
+ * Created by liuml on 2021/10/28 10:40
+ */
+public class CircleData {
+
+    private int startAngle;
+    private int endAngle;
+    private int color;
+    private String name;
+
+
+    public int getStartAngle() {
+        return startAngle;
+    }
+
+    public void setStartAngle(int startAngle) {
+        this.startAngle = startAngle;
+    }
+
+    public int getEndAngle() {
+        return endAngle;
+    }
+
+    public void setEndAngle(int endAngle) {
+        this.endAngle = endAngle;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- app/src/main/java/com/example/myclockview/MyClockView.java
+++ app/src/main/java/com/example/myclockview/MyClockView.java
@@ -3,18 +3,15 @@ package com.example.myclockview;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Matrix;
 import android.graphics.Paint;
-import android.graphics.Path;
 import android.graphics.PointF;
+import android.graphics.Rect;
 import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.View;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Created by liuml on 2021/10/27 13:38
@@ -34,22 +31,32 @@ public class MyClockView extends View {
         super(context, attrs, defStyleAttr);
     }
 
-    private int radius = 0;//半径
+    private int radius = 400;//半径 后面动态
+
+    private Paint mOutCirclePaint = null;//外层圆画笔
+    private Paint textPaint = null;//文字画笔
+    private Paint linePaint = null;//刻度画笔
+
+    /**
+     * 用于测量文本的宽、高度（这里主要是来获取高度）
+     */
+    private Rect textBounds = new Rect();
+
+
     private Paint mPaint = null;
-    private Paint StokPaint = null;
 
     private int mWidth = 0, mHeight = 0;
     private int padding;
     private PointF center = new PointF();
 
 
+    private String[] clockNumbers24 = {"24","13","14","15","16","17","18","19","20","21","22","23",};
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
 //        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        int width = getMeasureSize(true, widthMeasureSpec);
-        int height = getMeasureSize(false, heightMeasureSpec);
-        setMeasuredDimension(width, height);
-
+        mWidth = getMeasureSize(true, widthMeasureSpec);
+        mHeight = getMeasureSize(false, heightMeasureSpec);
+        setMeasuredDimension(mWidth, mHeight);
     }
 
     /**
@@ -89,21 +96,50 @@ public class MyClockView extends View {
     @Override
     protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
         super.onLayout(changed, left, top, right, bottom);
-        mPaint = new Paint();
-        mPaint.setAntiAlias(true);
-        StokPaint = new Paint();
-        StokPaint.setAntiAlias(true);
+
+        initPaint();
 
         if (mHeight == 0) {
             mWidth = right - left;
             mHeight = bottom - top;
-            padding = Math.min(mWidth, mHeight) / 20;
-            radius = Math.min(mWidth, mHeight) / 2 - padding;
+//            padding = 130;
+//            padding = Math.min(mWidth, mHeight) / 5;
+            padding = Math.min(mWidth, mHeight) / 10;
+//            radius = Math.min(mWidth, mHeight) / 2 - padding;
         }
 
         center.x = (left + right) / 2;
         center.y = (bottom - top) / 2;
 
+    }
+
+    private void initPaint() {
+        //绘制
+        /**
+         * Paint.Style.FILL设置只绘制图形内容
+         * Paint.Style.STROKE设置只绘制图形的边
+         * Paint.Style.FILL_AND_STROKE设置都绘制
+         */
+//        mOutCirclePaint.setStyle(Paint.Style.STROKE);
+//        mOutCirclePaint.setStrokeWidth(60f);//设置线宽
+
+        mOutCirclePaint = new Paint();
+        mOutCirclePaint.setAntiAlias(true);//设置Paint为无锯齿
+
+        textPaint = new Paint();
+        textPaint.setTextSize(30);
+        textPaint.setAntiAlias(true);//设置Paint为无锯齿
+        textPaint.setColor(getContext().getResources().getColor(R.color.gary));
+
+
+        linePaint = new Paint();
+        linePaint.setAntiAlias(true);//设置Paint为无锯齿
+        linePaint.setColor(getContext().getResources().getColor(R.color.gary));//设置灰色
+//        textPaint.setColor(708090);
+
+
+//        StokPaint = new Paint();
+//        StokPaint.setAntiAlias(true);
     }
 
     private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
@@ -120,104 +156,81 @@ public class MyClockView extends View {
         super.onDraw(canvas);
         //先设置画布为view的中心点  把当前画布的原点移到(center.x,center.y),后面的操作都以(10,10)作为参照点，默认原点为(0,0)
         canvas.save();
-        canvas.translate(center.x, center.y);
+
+        //画刻度和文字
+        drawOutText(canvas);
+        //画最外面的圆
         drawOutCircle(canvas);
-
-//        mPaint.setColor(0xff303F8F);
-//        canvas.drawCircle(center.x, center.y, radius, mPaint);//背景
-//
-//        Matrix matrix = new Matrix();
-//        mPaint.setColor(Color.LTGRAY);
-//        mPaint.setStrokeWidth(3);
-
-//        for (int i = 0; i < 60; i++) {
-//            matrix.setRotate(i * 6, center.x, center.y);//以绘制区域中心为远点旋转，并将旋转角度递增，达到转圈的效果
-//            canvas.setMatrix(matrix);
-//            if (i % 15 == 0) {
-//                //最大的刻度，3 6 9 12
-//                canvas.drawRect(center.x - 2, padding, center.x + 2, padding + 40, mPaint);
-//            } else if (i % 5 == 0) {
-//                //小一点的刻度  对应1、2、4、5...
-//                canvas.drawRect(center.x - 1.8f, padding, center.x + 1.8f, padding + 24, mPaint);
-//            } else {
-//                //分针对应的刻度
-//                canvas.drawRect(center.x - 1f, padding, center.x + 1f, padding + 12, mPaint);
-//            }
-//        }
-//        matrix.setRotate(0, center.x, center.y);//以绘制区域中心为远点旋转，并将旋转角度递增，达到转圈的效果
-//        canvas.setMatrix(matrix);
-//
-//        mPaint.setTextAlign(Paint.Align.CENTER);
-//        for (int i = 0; i < 4; i++) {//画4个数字  3 6 9 12
-//            //此处注意drawtex的y坐标，对应的是descent，不是top，也不是bottom
-//            String txt = String.valueOf(i == 0 ? 12 : i * 3);
-//            mPaint.setTextSize(40);
-//            float w = mPaint.measureText(txt);
-//            Paint.FontMetrics fm = mPaint.getFontMetrics();
-//            float des = fm.descent - fm.top;
-//            float x = 0;
-//            float y = 0;
-//            switch (i) {
-//                case 0:
-//                    x = center.x;
-//                    y = padding + 45 + des;
-//                    break;
-//                case 1:
-//                    x = center.x + radius - 45 - w;
-//                    y = center.y + (fm.bottom - fm.top) / 4;
-//                    break;
-//                case 2:
-//                    x = center.x;
-//                    y = center.y + radius - 45 - (fm.bottom - fm.top) / 2;
-//                    break;
-//                default:
-//                    x = center.x - radius + 45 + w;
-//                    y = center.y + (fm.bottom - fm.top) / 4;
-//                    break;
-//            }
-//            canvas.drawText(txt, x, y, mPaint);//画时刻数字
-//        }
-//
-////        Path path = new Path();
-////        path.addArc(new RectF(center.x - 200, padding + 150, center.x + 200, center.y - 150),
-////                180, 180);//设置扇形区域边框，以及旋转的角度
-////        Paint citePaint = new Paint(mPaint);
-////        citePaint.setTextSize(40);
-////        citePaint.setStrokeWidth(3);
-////        canvas.drawPath(path, citePaint);//可放开查看扇形区域
-//////        canvas.drawTextOnPath("lock view from canvas", path, 0, 0, citePaint);//画顶部扇形区域外边框文字
-////        canvas.drawText(dateFormat.format(new Date()), center.x, center.y + radius - 150, mPaint);//当前时间  也可以加上星期
-//
-//        mPaint.setColor(Color.BLACK);
-//        mPaint.setStyle(Paint.Style.STROKE);
-//        mPaint.setStrokeWidth(3);
-//        canvas.drawCircle(center.x, center.y, radius, mPaint); //画最外层圆圈，可以优化
-//        mPaint.setStyle(Paint.Style.FILL);
-//
-//        canvas.drawCircle(center.x, center.y, 15, mPaint);//这个圆是为了美观
 
         canvas.restore();//把当前画布返回（调整）到上一个save()状态之前
     }
 
-    private void drawOutCircle(Canvas canvas) {
-        //画最外面的圆
+    //画外面的时间文字 与刻度
+    private void drawOutText(Canvas canvas) {
 
+//            通过旋转画布的方式快速设置刻度
+//        canvas.rotate(6);
+
+        //x，y才是文本真正的准确坐标，需要减去文本的自身宽、高因素
+        int x,y;
+//        textPaint.setColor(getContext().getResources().getColor(R.color.gary));
+        for (int i = 0; i <= clockNumbers24.length-1; i++) {
+
+            //画刻度 与文字
+            drawText(canvas, clockNumbers24[i]);
+
+            textPaint.setStrokeWidth(20);
+            canvas.drawLine(center.x, center.y - radius, center.x, center.y - radius - 20, linePaint);
+//            canvas.drawText(i + "", center.x , center.y - radius - 35, textPaint);
+//            canvas.drawLine(0,-mClockRadius+mClockRingWidth/2,
+//                    0,-mClockRadius+mSpecialLength,mCirclePaint);
+
+            //计算画布每次需要旋转的角度
+            canvas.rotate(360/clockNumbers24.length, getWidth() / 2, getHeight() / 2);//以圆中心进行旋转
+        }
+
+
+        //绘制完后，把画布状态复原
+        canvas.restore();
+    }
+
+    private void drawText(Canvas canvas, String string) {
+        mPaint.getTextBounds(string, 0, string.length(), textBounds);
+//        x = (int) (preX - mPaint.measureText(string) / 2);
+//        y = preY - textBounds.height();//从文本的中心点处开始绘制
+
+        canvas.drawText(string, center.x , center.y - radius - 35, textPaint);
+
+    }
+
+    private void drawOutCircle(Canvas canvas) {
+
+//        canvas.translate(center.x, center.y);
+
+        mOutCirclePaint.setColor(Color.BLUE);
+        //绘制
+        /**
+         * Paint.Style.FILL设置只绘制图形内容
+         * Paint.Style.STROKE设置只绘制图形的边
+         * Paint.Style.FILL_AND_STROKE设置都绘制
+         */
+//        mOutCirclePaint.setStyle(Paint.Style.STROKE);
+//        mOutCirclePaint.setStrokeWidth(60f);//设置线宽
+
+        canvas.drawCircle(center.x, center.y, radius, mOutCirclePaint);//画大圆
 //        canvas.drawCircle(0,0,);
+
 
     }
 
     /**
-     * 获取今天的开始时间
+     * 获取当前时间
      *
      * @return 时间戳long
      */
     public static long getNowTime() {
-        Calendar c = Calendar.getInstance();
-        c.set(Calendar.HOUR_OF_DAY, 0);
-        c.set(Calendar.MINUTE, 0);
-        c.set(Calendar.SECOND, 0);
-        c.set(Calendar.MILLISECOND, 0);
-        return System.currentTimeMillis() - c.getTimeInMillis();
+
+        return System.currentTimeMillis();
     }
 
 
